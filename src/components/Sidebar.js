66fcd820487d1b91435ec64d3ad17d08baf21671
Moncace1983 <cesarmontoya212@gmail.com>
2025-03-04 14:18:09 -0500

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -25,6 +25,7 @@ const Sidebar = ({ isVisible, toggleSidebar }) => {
             <li><Link to="/gestion-ordenes">Gestión Órdenes</Link></li>
             <li><Link to="/gestion-productos">Gestión Productos</Link></li>
             <li><Link to="/informes">Informes</Link></li>
+            <li><button onClick={handleLogout}>Cerrar Sesión</button></li>
           </ul>
         </nav>
       )}
